300e12bf68208028975acff486bf32c140f96d00
David Stroud <david@greymatter-ag.com>
2015-12-26 20:24:18 -0600

    add template environment that allows users to login/logout

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import datetime
+import jinja2
+import os
 import webapp2
+
+from google.appengine.api import users
+
+template_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.getcwd()))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        message = '<p>The time is: %s</p>' % datetime.datetime.now()
-        self.response.out.write(message)
+        current_time = datetime.datetime.now()
+        user = users.get_current_user()
+        login_url = users.create_login_url(self.request.path)
+        logout_url = users.create_logout_url(self.request.path)
+
+        template = template_env.get_template('home.html')
+        context = {
+            'current_time': current_time,
+            'user': user,
+            'login_url': login_url,
+            'logout_url': logout_url,
+            }
+        self.response.out.write(template.render(context))
 
 application = webapp2.WSGIApplication([('/', MainPage)],
                                       debug=True)
